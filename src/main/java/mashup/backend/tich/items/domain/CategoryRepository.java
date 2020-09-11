c993157535c04c78e2e566648fefe18a13bef4c3
YJeongKim <yjeong20s@naver.com>
2020-09-12 04:59:33 +0900

    fix : Conflict 해결

--- src/main/java/mashup/backend/tich/items/domain/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package mashup.backend.myeonvely.item.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface CategoryRepository extends JpaRepository<Category, Long> {
-    Optional<Category> findByName(String name);
-}
